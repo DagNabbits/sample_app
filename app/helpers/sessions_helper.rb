b7a825304b89481b5d0a464f65982e4a0f8834bf
Dag Torgersbraten <dagtorgersbraten@gmail.com>
2013-12-19 17:43:27 +0000

    sigin inout and cucumber

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,14 +1,14 @@
 module SessionsHelper
 
-	 def signed_in?
-	 	!current_user.nil?
-	 end
-	 
 	 def  sign_in(user)
 	 	 cookies.permanent[:remember_token]= user.remember_token
 	 	 current_user = user
 	 end
 
+	 def signed_in?
+	 	!current_user.nil?
+	 end
+	 
 	 def current_user(user)
 	 	@current_user = user
 	 end
